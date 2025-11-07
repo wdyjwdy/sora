4dc39561110704751cd13c180b985ae37733781a
wdyjwdy <xxccat@gmail.com>
2025-11-07 12:58:05 +0800

    update

--- build/serve.ts
+++ build/serve.ts
@@ -1,87 +1,14 @@
-import { log, buildContentFile, buildStaticFile } from "./build.ts";
+import { buildContentFile, buildStaticFile, log } from "./utils.ts";
 import { join } from "node:path";
 import { watch } from "fs";
-import type { ServerWebSocket } from "bun";
-import config from "./config";
+import config from "./config.ts";
 
-type Socket = ServerWebSocket<{ pathname: string }>;
+await log();
 
-const clients = new Map<string, Socket>();
+watch(config.contentPath, { recursive: true }, async (_, filename) => {
+  await buildContentFile(join(config.contentPath, filename!));
+});
 
-async function serve(port = 3333) {
-  await log(true);
-
-  Bun.serve({
-    port,
-    websocket: {
-      // when open or refresh a page
-      open(ws: Socket) {
-        clients.set(ws.data.pathname, ws);
-      },
-      message() {},
-    },
-    async fetch(req, server) {
-      // get pathname
-      const pathname = new URL(req.url).pathname;
-      let filepath;
-      if (pathname === "/") {
-        filepath = join(config.outputPath, "index.html");
-      } else if (pathname.startsWith("/sora/")) {
-        filepath = join(config.outputPath, pathname.slice(5));
-      } else {
-        filepath = join(config.outputPath, pathname + ".html");
-      }
-
-      // update socket
-      const success = server.upgrade(req, {
-        data: {
-          pathname,
-        },
-      });
-      if (success) return;
-
-      // response page
-      const suffix = pathname.split(".").at(-1);
-      let type;
-      if (suffix === "html") {
-        type = "text/html";
-      } else if (suffix === "css") {
-        type = "text/css";
-      } else if (suffix === "js") {
-        type = "text/javascript";
-      } else if (suffix === "svg") {
-        type = "image/svg+xml";
-      }
-      const file = Bun.file(filepath, { type });
-      if (!(await file.exists())) {
-        return new Response("Not Found", { status: 404 });
-      }
-      return new Response(file);
-    },
-  });
-
-  watch(config.contentPath, { recursive: true }, async (_, filename) => {
-    await buildContentFile(join(config.contentPath, filename!), true);
-    const pathname = "/" + filename?.replace(".md", "");
-
-    const ws = clients.get(pathname);
-    if (ws?.readyState === 1) {
-      ws.send("reload");
-    }
-    console.log(`\x1b[34m[sora]\x1b[0m rebuild \x1b[37m${filename}\x1b[0m`);
-  });
-
-  watch(config.staticPath, { recursive: true }, async (_, filename) => {
-    await buildStaticFile(join(config.staticPath, filename!));
-    for (let ws of clients.values()) {
-      if (ws?.readyState === 1) {
-        ws.send("reload");
-      }
-    }
-    console.log(`\x1b[34m[sora]\x1b[0m rebuild \x1b[37m${filename}\x1b[0m`);
-  });
-
-  console.log(`\x1b[34m[sora]\x1b[0m listen \x1b[37mlocalhost:${port}\x1b[0m`);
-}
-
-serve();
+watch(config.staticPath, { recursive: true }, async (_, filename) => {
+  await buildStaticFile(join(config.staticPath, filename!));
+});
---
+++ build/utils.ts
@@ -0,0 +1,128 @@
+import { readdir } from "node:fs/promises";
+import { join } from "node:path";
+import { Buffer } from "node:buffer";
+import minifyHtml from "@minify-html/node";
+import Handlebars from "handlebars";
+import { parse } from "yaml";
+import { highlight } from "./highlight";
+import config from "./config";
+import MarkdownIt from "markdown-it";
+import MarkdownItAnchor from "markdown-it-anchor";
+import MarkdownItTOC from "markdown-it-table-of-contents";
+
+// 初始化 Markdown Parser
+const mdit = MarkdownIt({ highlight })
+  .use(MarkdownItAnchor)
+  .use(MarkdownItTOC, { includeLevel: [2, 3] });
+
+// 初始化搜索
+const pages: Array<{
+  url: string;
+  title: string;
+  category: string;
+  alias?: string[];
+}> = [];
+const categories = new Set<string>();
+const contentPaths = await getFilenames(config.contentPath);
+for (let path of contentPaths) {
+  if (!path.endsWith(".md")) continue;
+  const url = path.slice(8, -3);
+  const { frontmatter } = await parseContent(path);
+  const { title, category, alias } = frontmatter;
+  pages.push({ url, title, category, alias });
+  categories.add(category);
+}
+pages.sort((a, b) => (a.category < b.category ? 1 : -1));
+
+// 初始化模版
+const templateFile = Bun.file(config.templatePath);
+const templateText = await templateFile.text();
+const templateFunc = Handlebars.compile(templateText);
+Handlebars.registerHelper("pages", function () {
+  return JSON.stringify(pages);
+});
+Handlebars.registerHelper("categories", function (options) {
+  return [...categories]
+    .sort()
+    .map((i) => options.fn(i))
+    .join("");
+});
+
+async function parseContent(path: string) {
+  const file = Bun.file(path);
+  const text = await file.text();
+  const match = text.match(/^---(.*?)---/s);
+  return {
+    frontmatter: match ? parse(match[1]) : {},
+    markdown: match ? text.slice(match[0].length) : text,
+  };
+}
+
+function addPrefix(html: string) {
+  html = html.replace(/(?<=img src=")(.+?)(?=")/g, "/static/imgs/$1.svg");
+  if (process.env.env === "prod") {
+    html = html.replace(/\/static\//g, `${config.assetPrefix}static/`);
+  }
+  return html;
+}
+
+function minify(html: string) {
+  return minifyHtml.minify(Buffer.from(html), {});
+}
+
+async function buildContentFile(path: string) {
+  if (path.endsWith("index.html")) {
+    const file = Bun.file(path);
+    const text = await file.text();
+    const html = Handlebars.compile(text)({});
+    await Bun.write(config.outputPath + "/index.html", minify(addPrefix(html)));
+    return;
+  }
+
+  // build
+  let { frontmatter, markdown } = await parseContent(path);
+  if (frontmatter.toc) markdown = "[[toc]]\n" + markdown;
+  let html = mdit.render(markdown);
+  html = templateFunc({ content: html, ...frontmatter });
+  html = addPrefix(html);
+
+  // save
+  const outputPath = path
+    .replace(config.contentPath, config.outputPath)
+    .replace(".md", ".html");
+  await Bun.write(outputPath, minify(html));
+}
+
+async function buildStaticFile(path: string) {
+  const file = Bun.file(path);
+  const outputPath = join(config.outputPath, path);
+  Bun.write(outputPath, file);
+}
+
+async function getFilenames(path: string) {
+  const paths = await readdir(path, {
+    recursive: true,
+    withFileTypes: true,
+  });
+  return paths
+    .filter((path) => path.isFile())
+    .map((path) => join(path.parentPath, path.name));
+}
+
+async function build() {
+  const contentPaths = await getFilenames(config.contentPath);
+  await Promise.all(contentPaths.map((path) => buildContentFile(path)));
+
+  const staticPaths = await getFilenames(config.staticPath);
+  await Promise.all(staticPaths.map((path) => buildStaticFile(path)));
+}
+
+async function log() {
+  const start = Bun.nanoseconds();
+  await build();
+  const end = Bun.nanoseconds();
+  const time = ((end - start) / 1_000_000).toFixed();
+  console.log(`\x1b[34m[sora]\x1b[0m build \x1b[37m${time}ms\x1b[0m`);
+}
+
+export { log, buildContentFile, buildStaticFile };
